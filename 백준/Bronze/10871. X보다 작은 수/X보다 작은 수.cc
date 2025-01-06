cab60f69f861fec58349c27f9c17de682823b1f6
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-06 23:00:53 +0900

    [Bronze V] Title: X보다 작은 수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    long int X;
+    cin>>N>>X;
+    int arr[N];
+    for(int i=0; i<N; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0; i<N; i++)
+    {
+        if(arr[i]<X) cout<<arr[i]<<" ";
+    }
+    return 0;
+}
