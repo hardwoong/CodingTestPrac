a2928e6c8a9e9ef5439d6054a45d94e1df6076c5
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-10 00:02:44 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1440 ms, Memory: 163652 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Collections;
 import java.util.Vector;
 
@@ -10,18 +8,27 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+
+    // 입력 크기
     int size = Integer.parseInt(br.readLine());
     Vector<Integer> v = new Vector<>();
+
+    // 입력된 정수 추가
     for (int i = 0; i < size; i++) {
       v.add(Integer.parseInt(br.readLine()));
     }
     br.close();
+
+    // 오름차순 정렬
     Collections.sort(v);
+
+    // 정렬된 데이터 StringBuilder에 추가
     for (int i = 0; i < size; i++) {
-      bw.write(String.valueOf(v.get(i)) + "\n");
+      sb.append(v.get(i)).append("\n");
     }
-    bw.flush();
-    bw.close();
+
+    // 결과 출력
+    System.out.print(sb);
   }
 }
