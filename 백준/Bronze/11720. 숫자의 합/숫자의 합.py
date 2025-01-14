bb2b2e51834908e7f6ef00c8cb343ab6ce5158fd
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-14 12:30:34 +0900

    [Bronze IV] Title: 숫자의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,7 +1,4 @@
-N = int(input())
-num = input()
-sum = 0
-for i in range(1, len(num) + 1):
-  sum += int(num[i - 1])
+N = input()
+numbers = list(map(int, input()))
 
-print(sum)
+print(sum(numbers))
