5157fb9a9653f827ace2c39330e3afb257d2292a
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-22 17:39:06 +0900

    [Silver V] Title: 너의 평점은, Time: 112 ms, Memory: 14624 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -10,39 +10,22 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     double sum = 0.0;
-    double hak = 0.0;
+    double totalSum = 0.0;
+    String grades[] = {"A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F"};
+    double scores[] = {4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0};
     for (int i = 0; i < 20; i++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
       String sub = st.nextToken();
       double score = Double.parseDouble(st.nextToken());
       String grade = st.nextToken();
-      if (grade.equals("P")) {
-        continue;
-      }
-      
-      hak += score;
-      
-      if (grade.equals("A+")) {
-        sum += 4.5 * score;
-      } else if (grade.equals("A0")) {
-        sum += 4.0 * score;
-      } else if (grade.equals("B+")) {
-        sum += 3.5 * score;
-      } else if (grade.equals("B0")) {
-        sum += 3.0 * score;
-      } else if (grade.equals("C+")) {
-        sum += 2.5 * score;
-      } else if (grade.equals("C0")) {
-        sum += 2.0 * score;
-      } else if (grade.equals("D+")) {
-        sum += 1.5 * score;
-      } else if (grade.equals("D0")) {
-        sum += 1.0 * score;
-      } else if (grade.equals("F")) {
-        sum += 0.0 * score;
+      for (int j = 0; j < grades.length; j++) {
+        if (grade.equals(grades[j])) {
+          sum += score;
+          totalSum += scores[j] * score;
+        }
       }
     }
-    bw.write(String.format("%.6f\n", sum / hak));
+    bw.write(String.format("%.6f\n", totalSum / sum));
     bw.flush();
     bw.close();
   }
