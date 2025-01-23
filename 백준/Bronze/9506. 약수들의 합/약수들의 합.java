887bedbf306e49ac36c8682ec7ccfb0c1f308e77
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-23 14:15:10 +0900

    [Bronze I] Title: 약수들의 합, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -3,34 +3,48 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+
     while (true) {
       int n = Integer.parseInt(br.readLine());
       if (n == -1) break;
+
+      ArrayList<Integer> divisors = new ArrayList<>();
       int sum = 0;
-      Vector<Integer> v = new Vector<>();
-      for (int i = 1; i < n; i++) {
+
+      for (int i = 1; i <= Math.sqrt(n); i++) {
         if (n % i == 0) {
-          v.add(i);
           sum += i;
+          divisors.add(i);
+
+          if (i != 1 && i != n / i) {
+            sum += n / i;
+            divisors.add(n / i);
+          }
         }
       }
+
       if (sum == n) {
-        bw.write(String.format("%d = ", n));
-        for (int i = 0; i < v.size(); i++) {
-          bw.write(String.valueOf(v.get(i)));
-          if (i != v.size() - 1) bw.write(" + ");
-          else bw.write("\n");
+        sb.append(n).append(" = ");
+        divisors.sort(Integer::compareTo); 
+        for (int i = 0; i < divisors.size(); i++) {
+          sb.append(divisors.get(i));
+          if (i < divisors.size() - 1) sb.append(" + ");
         }
+        sb.append("\n");
       } else {
-        bw.write(String.format("%d is NOT perfect.\n", n));
+        sb.append(n).append(" is NOT perfect.\n");
       }
     }
+
+    bw.write(sb.toString());
     bw.flush();
+    bw.close();
   }
 }
