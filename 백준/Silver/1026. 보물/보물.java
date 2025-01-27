162a95977d51e773f1759e715374795497219b6e
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-27 22:56:12 +0900

    [Silver IV] Title: 보물, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,35 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     int n = Integer.parseInt(br.readLine());
-    ArrayList<Integer> A = new ArrayList<>();
-    ArrayList<Integer> B = new ArrayList<>();
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] A = new int[n];
+    int[] B = new int[n];
+
+    String[] inputA = br.readLine().split(" ");
+    String[] inputB = br.readLine().split(" ");
+
     for (int i = 0; i < n; i++) {
-      A.add(Integer.parseInt(st.nextToken()));
+      A[i] = Integer.parseInt(inputA[i]);
+      B[i] = Integer.parseInt(inputB[i]);
     }
 
-    st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < n; i++) {
-      B.add(Integer.parseInt(st.nextToken()));
-    }
+    // A를 오름차순 정렬
+    Arrays.sort(A);
 
-    A.sort(Integer::compareTo);
-    B.sort(Collections.reverseOrder());
+    // B를 내림차순 정렬
+    Arrays.sort(B);
+    for (int i = 0; i < n / 2; i++) {
+      int temp = B[i];
+      B[i] = B[n - 1 - i];
+      B[n - 1 - i] = temp;
+    }
 
     int result = 0;
     for (int i = 0; i < n; i++) {
-      result += A.get(i) * B.get(i);
+      result += A[i] * B[i];
     }
 
     System.out.println(result);
