9a117cb1a6daf9752d0f930069bef179cc76c7ab
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-29 23:36:40 +0900

    [Silver IV] Title: 동전 0, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,28 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String inps[] = br.readLine().split(" ");
-    int N = Integer.parseInt(inps[0]);
-    int K = Integer.parseInt(inps[1]);
-    int coins[] = new int[N];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] coin = new int[N];
+		
+		//동전의 가치들을 배열에 저장
+		for(int i = 0; i < N; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+		}
+		
+		//조건에 충족하지 못할 경우 다시 K값을 되돌려야 하기때문에 따로 변수를 만든다.
+		int now = K;
+		int result = 0;
+		//원하는 가치의 합이 딱 맞을때까지 반복
+		while(now != 0) {
+			for(int i = N - 1; i >= 0; i--) {
+				if(now >= coin[i]) {
+					result += (now / coin[i]);
+					now %= coin[i];
+					if(now < coin[0] && now != 0) {
+						result = 0;
+						now = K;
+					}
+				}
+			}
+		}
+		System.out.println(result);
+	}
 
-    for (int i = 0; i < N; i++) {
-      coins[i] = Integer.parseInt(br.readLine());
-    }
-
-    int cnt = 0; 
-
-    for (int i = N - 1; i >= 0; i--) {
-      if (coins[i] <= K) {  
-        cnt += K / coins[i];  
-        K %= coins[i];        
-      }
-    }
-
-    System.out.println(cnt); 
-  }
 }
+
