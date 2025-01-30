7bc8cc63b35966361649d5394ad8cc7a8f67b436
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-01-31 01:26:23 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,25 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-
-            if (a == b && b == c) {
-                if (a == 0)
-                    break;
-                System.out.println("Equilateral");
-            } else if (a + b <= c || b + c <= a || c + a <= b) {
-                System.out.println("Invalid");
-            } else if (a != b && b != c && c != a) {
-                System.out.println("Scalene");
-            } else {
-                System.out.println("Isosceles");
-            }
-        }
-        sc.close();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    while (true) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int nums[] = new int[3];
+      nums[0] = Integer.parseInt(st.nextToken());
+      nums[1] = Integer.parseInt(st.nextToken());
+      nums[2] = Integer.parseInt(st.nextToken());
+      Arrays.sort(nums);
+      if (nums[2] == 0) break;
+      else if (nums[0] == nums[1] && nums[1] == nums[2]) bw.write("Equilateral\n");
+      else if (nums[0] == nums[1] && (nums[0] + nums[1]) > nums[2]) bw.write("Isosceles\n");
+      else if (nums[1] == nums[2] && (nums[0] + nums[1]) > nums[2]) bw.write("Isosceles\n");
+      else if ((nums[0] + nums[1]) > nums[2]) bw.write("Scalene\n");
+      else bw.write("Invalid\n");
     }
+    bw.flush();
+  }
 }
