413d6b1b73610161bf603b6d6dea30a0bcb1955b
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-02-03 22:29:09 +0900

    [Silver I] Title: 안정적인 문자열, Time: 128 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.java
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.java
@@ -1,36 +1,34 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.OutputStreamWriter;
 
 public class Main {
+  private static final char OPEN = '{';
+  // private static final char CLOSE = '}';
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int t = 1;
-
-    while (true) {
-      String line = br.readLine();
-      if (line.contains("-")) break; 
-      
-      Stack<Character> s = new Stack<>();
-      int count = 0;
-
-      for (char ch : line.toCharArray()) {
-        if (ch == '{') {
-          s.push(ch); 
-        } else {
-          if (s.isEmpty()) { 
-            count++; 
-            s.push('{'); 
-          } else {
-            s.pop();
-          }
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int num = 1;
+    while(true){
+      String command = br.readLine();
+      if(command.charAt(0) == '-') break;
+      int cnt = 0;
+      int result = 0;
+      for(int i = 0 ; i < command.length() ; i++) {
+        char ch = command.charAt(i);
+        if(ch == OPEN) cnt++;
+        else cnt--;
+        if(cnt < 0){
+          result++;
+          cnt += 2;
         }
       }
-
-      count += s.size() / 2;
-      System.out.println(t + ". " + count);
-      t++;
+      if(cnt > 0) result += cnt / 2;
+      bw.write(String.format("%d. %d\n", num, result));
+      num++;
     }
+    bw.flush();
   }
 }
