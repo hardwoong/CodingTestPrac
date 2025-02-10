0aef512989d1edc5e8e2f8f23b37c91ba584121a
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-02-10 16:03:11 +0900

    [Gold III] Title: 크게 만들기, Time: 372 ms, Memory: 34528 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -9,40 +9,33 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     
-    int N = Integer.parseInt(st.nextToken()); // 숫자의 길이
-    int K = Integer.parseInt(st.nextToken()); // 제거할 숫자의 개수
-    String num = br.readLine(); // 입력 숫자
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken()); 
+    String nums = br.readLine();
 
     Stack<Character> stack = new Stack<>();
 
-    int removeCount = K; // 제거할 횟수
+    for (int i = 0; i < n; i++) {
+      char num = nums.charAt(i);
 
-    // 숫자를 하나씩 확인
-    for (int i = 0; i < N; i++) {
-      char digit = num.charAt(i);
-
-      // 스택이 비어있지 않고, K개의 숫자를 제거할 수 있으며,
-      // 현재 숫자가 스택 top보다 크다면 pop 진행
-      while (!stack.isEmpty() && removeCount > 0 && stack.peek() < digit) {
+      while (!stack.isEmpty() && k > 0 && stack.peek() < num) {
         stack.pop();
-        removeCount--;
+        k--;
       }
 
-      stack.push(digit);
+      stack.push(num);
     }
 
-    // 남아있는 제거 횟수만큼 뒤에서 제거
-    while (removeCount > 0) {
+    while (k > 0) {
       stack.pop();
-      removeCount--;
+      k--;
     }
 
-    // 결과 출력
     StringBuilder sb = new StringBuilder();
     while (!stack.isEmpty()) {
       sb.append(stack.pop());
     }
 
-    System.out.println(sb.reverse().toString()); // 스택이므로 뒤집어서 출력
+    System.out.println(sb.reverse().toString());
   }
 }
