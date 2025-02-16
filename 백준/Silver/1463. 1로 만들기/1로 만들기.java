4dd8de4a8f0fc9cf0b329de73acb6342f51c3746
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-02-16 21:37:07 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,24 +1,22 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-
+import java.io.IOException;
+ 
+ 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int n = Integer.parseInt(br.readLine());
-    int arr[] = new int[n + 1];
-    arr[1] = 0;
-    for (int i = 2; i <= n; i++) {
-      arr[i] = arr[i - 1] + 1;
-      if (i % 2 == 0) {
-        arr[i] = Math.min(arr[i], arr[i / 2] + 1);
-      }
-      if (i % 3 == 0) {
-        arr[i] = Math.min(arr[i], arr[i / 3] + 1);
-      }
-    }
-    System.out.println(arr[n]);
-  }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(recur(N, 0));
+	}
+ 
+	static int recur(int N, int count) {
+		// N이 2 미만인 경우 누적된 count값을 반환
+		if (N < 2) {
+			return count;
+		}
+		return Math.min(recur(N / 2, count + 1 + (N % 2)), recur(N / 3, count + 1 + (N % 3)));
+ 
+	}
 }
