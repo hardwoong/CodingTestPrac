b63e2ec3f974dcb5b017ff6b1231164dfce22066
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-02-25 14:59:20 +0900

    [Silver II] Title: 한 줄로 서기, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,24 +1,25 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     int arr[] = new int[n];
-    for (int i = 0; i < n; i++) { arr[i] = Integer.parseInt(st.nextToken()); }
-    ArrayList<Integer> al = new ArrayList<>();
-    StringBuilder sb = new StringBuilder();
-    for (int i = n - 1; i > -1; i--) {
-      al.add(arr[i], i + 1);
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
-    for (int i : al) {
-      sb.append(i + " ");
+
+    LinkedList<Integer> list = new LinkedList<>();
+    for (int i = n - 1; i >= 0; i--) {
+      list.add(arr[i], i + 1); 
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int num : list) {
+      sb.append(num).append(" ");
     }
     System.out.println(sb.toString());
   }
