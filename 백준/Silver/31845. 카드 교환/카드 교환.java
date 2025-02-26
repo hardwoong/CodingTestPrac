32d099ad7e029f1f059c54dd3606f2d61e418553
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-02-26 12:43:46 +0900

    [Silver III] Title: 카드 교환, Time: 108 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/31845. 카드 교환/카드 교환.java
+++ 백준/Silver/31845. 카드 교환/카드 교환.java
@@ -1,32 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        Arrays.sort(arr);
-        int ans = 0;
-        int score = 0;
-        
-        for (int i = N - 1; i >= score; i--) {
-            if (arr[i] > 0) {
-                ans += arr[i];
-                score += 1;
-            }
-            if (i == N - M) break;
-        }
-        
-        System.out.println(ans);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[] arr = new int[n];
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    // 오름차순 정렬
+    Arrays.sort(arr);
+
+    int ans = 0;
+    int banker = 0;
+
+    for (int i = n - 1; i >= banker; i--) {
+      if (arr[i] > 0) {
+        ans += arr[i];
+        banker += 1;
+      }
+      if (i == n - m) break;
+    }
+
+    System.out.println(ans);
+  }
 }
