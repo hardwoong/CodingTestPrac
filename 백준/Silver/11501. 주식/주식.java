663d00ac17a24570e2b833fc89a83e740fca6611
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-03-25 19:54:38 +0900

    [Silver II] Title: 주식, Time: 1028 ms, Memory: 315056 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,30 +1,47 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+ 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int t = Integer.parseInt(br.readLine());
-
-    while (t-- > 0) {
-      int n = Integer.parseInt(br.readLine());
-      int[] arr = new int[n];
-
-      String inps[] = br.readLine().split(" ");
-      for (int i = 0; i < n; i++) {
-        arr[i] = Integer.parseInt(inps[i]);
-      }
-      long profit = 0;
-      int max = 0;
-
-      for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] > max) {
-          max = arr[i];
-        } else {
-          profit += (max - arr[i]);
-        }
-      }
-
-      System.out.println(profit);
-    }
-  }
+	static int N, C, H, W, K, M, T;
+	static int[] arr;
+	static int answer = 0;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		T = Integer.parseInt(st.nextToken());
+		while(T-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			arr = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0;i<N;i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			sb.append(stock()).append("\n");
+		}
+		System.out.println(sb.toString());
+		
+	}
+	
+	static long stock() {
+		long max = arr[N-1];
+		long profit = 0;
+		for(int i=N-1; i>=0; i--) {
+			if(arr[i] <= max) {
+				profit += max - arr[i];
+			}else if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return profit;
+	}
+	
 }
