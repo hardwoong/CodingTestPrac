4c38ed8e06320bb3e2b965d72949c5a2036f58c3
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-03-25 16:22:32 +0900

    [Silver III] Title: 1 빼기, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/25709. 1 빼기/1 빼기.java
+++ 백준/Silver/25709. 1 빼기/1 빼기.java
@@ -1,53 +1,49 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String N = br.readLine();
+    int n = Integer.parseInt(br.readLine());
 
-    Queue<String> q = new LinkedList<>();
-    Set<String> visited = new HashSet<>();
+    int ans = 0;
 
-    q.offer(N);
-    visited.add(N);
+    while (true) {
+      boolean removedOne = false;
+      String str = String.valueOf(n);
 
-    int step = 0;
+      for (int i = 0; i < str.length(); i++) {
+        if (str.charAt(i) == '1') {
+          boolean flag = false;
+          StringBuilder temp = new StringBuilder();
 
-    while (!q.isEmpty()) {
-      int size = q.size();
-      for (int i = 0; i < size; i++) {
-        String cur = q.poll();
-        if (cur.equals("0")) {
-          System.out.println(step);
-          return;
-        }
+          for (int k = 0; k < str.length(); k++) {
+            if (k == i) continue;
 
-        // 1. 숫자에서 1을 뺀다
-        long val = Long.parseLong(cur);
-        if (val > 0) {
-          String sub1 = String.valueOf(val - 1);
-          if (!visited.contains(sub1)) {
-            visited.add(sub1);
-            q.offer(sub1);
-          }
-        }
-
-        // 2. 문자열에서 '1'을 제거하는 모든 경우
-        for (int j = 0; j < cur.length(); j++) {
-          if (cur.charAt(j) == '1') {
-            String removed = cur.substring(0, j) + cur.substring(j + 1);
-            // 앞의 0 제거
-            removed = removed.replaceFirst("^0+", "");
-            if (removed.equals("")) removed = "0";
-            if (!visited.contains(removed)) {
-              visited.add(removed);
-              q.offer(removed);
+            if (str.charAt(k) != '0') {
+              flag = true;
+            }
+            if (flag) {
+              temp.append(str.charAt(k));
             }
           }
+
+          str = temp.toString();
+          removedOne = true;
+          break;
         }
       }
-      step++;
+
+      ans++;
+
+      if (str.equals("")) break;
+
+      n = Integer.parseInt(str);
+
+      if (!removedOne) {
+        n -= 1;
+      }
     }
+
+    System.out.println(ans);
   }
 }
