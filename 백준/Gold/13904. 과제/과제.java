442f459e061094c4d2f751a3e3eae7f75c5406b1
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-04-01 22:33:06 +0900

    [Gold III] Title: 과제, Time: 120 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -2,58 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Assignment implements Comparable<Assignment> {
-        int deadline;
-        int score;
-
-        Assignment(int deadline, int score) {
-            this.deadline = deadline;
-            this.score = score;
-        }
-
-        @Override
-        public int compareTo(Assignment o) {
-            return o.score - this.score; // 점수 기준 내림차순 정렬
-        }
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[][] arr = new int[n][2];
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int d = Integer.parseInt(st.nextToken());
+      int w = Integer.parseInt(st.nextToken());
+      arr[i][0] = d;
+      arr[i][1] = w;
     }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        List<Assignment> assignments = new ArrayList<>();
-        int maxDeadline = 0;
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int d = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            assignments.add(new Assignment(d, w));
-            if (d > maxDeadline) {
-                maxDeadline = d;
-            }
+    Arrays.sort(arr, ((x, y) -> y[1] - x[1])); // 점수 위주 내림차순 정렬
+    boolean[] homework = new boolean[1001];
+    int point = 0;
+    for (int i = 0; i < arr.length; i++) {
+      int d = arr[i][0];
+      int w = arr[i][1];
+      for (int j = d; j > 0; j--) {
+        if (!homework[j]) {
+          point += w;
+          homework[j] = true;
+          break;
         }
-
-        // 마감일 기준 내림차순 정렬
-        assignments.sort((a1, a2) -> a2.deadline - a1.deadline);
-
-        PriorityQueue<Assignment> pq = new PriorityQueue<>();
-        int totalScore = 0;
-        int index = 0;
-
-        // 가장 늦은 마감일부터 1일까지 역순으로 탐색
-        for (int day = maxDeadline; day > 0; day--) {
-            // 해당 날짜에 수행할 수 있는 과제들을 우선순위 큐에 추가
-            while (index < n && assignments.get(index).deadline >= day) {
-                pq.offer(assignments.get(index));
-                index++;
-            }
-            // 우선순위 큐에서 가장 높은 점수의 과제를 선택하여 수행
-            if (!pq.isEmpty()) {
-                totalScore += pq.poll().score;
-            }
-        }
-
-        System.out.println(totalScore);
+      }
     }
+    System.out.println(point);
+  }
 }
