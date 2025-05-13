4013edd698b5e50f89557bc88c8cdf8b11e65950
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-05-14 00:50:29 +0900

    [Silver I] Title: 헨리, Time: 124 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Silver/10253. 헨리/헨리.java
+++ 백준/Silver/10253. 헨리/헨리.java
@@ -1,33 +1,40 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static int henry(int a, int b) {
-		int n = b / a;
-		if (n * a >= b) {
-			return n;
-		}
-		return n + 1;
-	}
+  public static int henry(int a, int b) {
+    int n = b / a;
+    if (n * a >= b) {
+      return n;
+    }
+    return n + 1;
+  }
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int testcase = in.nextInt();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		for (int i = 0; i < testcase; i++) {
-			int a = in.nextInt();
-			int b = in.nextInt();
+    int testcase = Integer.parseInt(br.readLine());
 
-			while (true) {
-				int x = henry(a, b);
-				
-				if (a * x == b) {
-					System.out.println(x);
-					break;
-				}
-				
-				a = a * x - b;
-				b = b * x;
-			}
-		}
-	}
+    for (int i = 0; i < testcase; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+
+      while (true) {
+        int x = henry(a, b);
+
+        if (a * x == b) {
+          bw.write(x + "\n");
+          break;
+        }
+
+        a = a * x - b;
+        b = b * x;
+      }
+    }
+    bw.flush();
+    bw.close();
+    br.close();
+  }
 }
