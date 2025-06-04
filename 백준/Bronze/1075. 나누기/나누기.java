c608bf2f259e66f141eab1035d940f298d6d9e97
hardwoong <158552165+hardwoong@users.noreply.github.com>
2025-06-04 18:04:36 +0900

    [Bronze II] Title: 나누기, Time: 112 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/1075. 나누기/나누기.java
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -3,17 +3,14 @@ import java.io.*;
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    long n = Long.parseLong(br.readLine());
+    long n_original = Long.parseLong(br.readLine());
     long f = Long.parseLong(br.readLine());
-    n = (n / 100) * 100; // 뒤 두 자리 00으로 만들기
-    long answer = 0;
-    for (long i = 0; i < 100; i++) {
-      if ((n + i) % f == 0) {
-        answer = i;
-        break;
-      }
-    }
-    System.out.printf("%02d\n", answer); // 두 자리로 출력
+
+    long n_base = (n_original / 100) * 100; // N의 마지막 두 자리를 00으로 만듦
+
+    long required_addition = (f - (n_base % f)) % f;
+
+    System.out.printf("%02d\n", required_addition); // 두 자리로 출력 (예: 0, 5, 12 -> "00", "05", "12")
     br.close();
   }
 }
